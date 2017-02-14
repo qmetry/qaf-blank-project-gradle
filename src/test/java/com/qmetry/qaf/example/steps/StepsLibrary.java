46d3a3f060031ecc85061fcd6896d36a7afa0b7e
Amit Bhoraniya <amit.bhoraniya@infostretch.com>
2017-02-14 17:40:56 +0530

    QAF Blank poject with gradle script

---
+++ src/test/java/com/qmetry/qaf/example/steps/StepsLibrary.java
@@ -0,0 +1,18 @@
+package com.qmetry.qaf.example.steps;
+
+import static com.qmetry.qaf.automation.step.CommonStep.click;
+import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
+
+import com.qmetry.qaf.automation.step.QAFTestStep;
+
+public class StepsLibrary {
+	/**
+	 * @param searchTerm
+	 *            : search term to be searched
+	 */
+	@QAFTestStep(description = "search for {0}")
+	public static void searchFor(String searchTerm) {
+		sendKeys(searchTerm, "input.search");
+		click("button.search");
+	}
+}
